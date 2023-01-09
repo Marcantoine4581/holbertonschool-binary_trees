8939146954b0947dc3903e54b542d1b3f022c73d
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-09 08:17:09 -0800

    task 12 correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,8 +13,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL && tree->right != NULL)
-		return (2);
-	else
-		return (1);
+
+	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->right));
 }
