63176c5e80f41d79a635f71903957ff1b5127fb2
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-09 01:44:33 -0800

    task 0 added + header file updated

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef _BINARY_TREE_H_
 #define _BINARY_TREE_H_
 
+#include <stdlib.h>
+#include <stddef.h>
+
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,6 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
