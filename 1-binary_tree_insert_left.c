7bd7d89be06c6d51d5ff8850caca40580ff39ba8
Tnozone <maxnocquet@gmail.com>
2023-01-09 02:16:37 -0800

    free new if parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL || parent == NULL)
+	if (new == NULL)
 		return (NULL);
+
+	if (parent == NULL)
+	{
+		free (new);
+		return (NULL);
+	}
 
 	if (parent->left != NULL)
 		parent = parent->left;
