723147f79eb815f1c097e95edd67ef6179042446
Tnozone <maxnocquet@gmail.com>
2023-01-10 01:48:37 -0800

    count nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,11 +8,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left || tree->right)
-		return (1);
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left != NULL || tree->right != NULL)
+		nodes += 1;
+
+	return (nodes);
 }
