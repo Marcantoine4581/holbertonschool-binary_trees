1f9a824f6ae87eb1d9d6f0685ce0ce0f02d7dd47
Tnozone <maxnocquet@gmail.com>
2023-01-10 00:50:23 -0800

    ajustments to function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height, left_side, right_side;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left_side;
-	size_t right_side;
 
 	left_side = binary_tree_height(tree->left);
 	right_side = binary_tree_height(tree->right);
 
 	if (left_side > right_side)
-		return (left_side + 1);
+		height = left_side + 1;
 	else
-		return (right_side + 1);
+		height = right_side + 1;
+
+	return (height);
 }
